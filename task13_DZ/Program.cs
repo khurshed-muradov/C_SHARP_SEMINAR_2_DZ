6886b6138e8e5fff7b09f01edcc8db0093269eb3
khurshed <hurshed@gmail.com>
2023-07-19 23:22:34 +0500

    Добавил решение Задачи №13 в ДЗ

--- task13_DZ/Program.cs
+++ task13_DZ/Program.cs
@@ -4,3 +4,29 @@
 // 78 -> Третьей цифры нет.
 // 32679 -> 6
 
+int EntNumber()
+{
+    int entNumber = int.Parse(Console.ReadLine()!);
+    return entNumber;
+}
+
+Console.WriteLine("Введите число: ");
+int enteredNumber = EntNumber();
+
+if (enteredNumber < 100)
+{
+    Console.WriteLine("Третей цифры нет!");
+}
+if (enteredNumber >= 100 && enteredNumber <= 999)
+{
+    Console.WriteLine(enteredNumber % 10);
+}
+if (enteredNumber >= 1000 && enteredNumber <= 9999)
+{
+    Console.WriteLine((enteredNumber / 10) % 10);
+}
+if (enteredNumber >= 10000 && enteredNumber <= 99999)
+{
+    Console.WriteLine((enteredNumber / 100) % 10);
+}
+
