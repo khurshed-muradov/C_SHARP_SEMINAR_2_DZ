e12db1929ff742ce1e5f93a309d137aabe90e987
khurshed <hurshed@gmail.com>
2023-07-19 21:56:14 +0500

    Добавил решение

--- task10_DZ/Program.cs
+++ task10_DZ/Program.cs
@@ -3,3 +3,21 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+int TripleNumber()
+{
+    int tripleNumber = int.Parse(Console.ReadLine()!);
+    return tripleNumber;
+}
+Console.WriteLine("Введите трехзнаxное число: ");
+int entTripleNumber = TripleNumber();
+
+int GetSecondDigit(int entTripleNumber)
+{
+
+    int secondDigit = (entTripleNumber / 10) % 10;
+    return secondDigit;
+}
+if (entTripleNumber < 100 || entTripleNumber > 999)
+    Console.WriteLine("Число не трехзначное");
+else
+    Console.WriteLine(GetSecondDigit(entTripleNumber));
